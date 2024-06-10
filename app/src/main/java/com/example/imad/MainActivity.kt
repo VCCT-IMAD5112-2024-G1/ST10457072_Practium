7da0747a0882e7b41b939cc08194542c9c9bdf0a
lab_services_student <ST10457072@vcconnect.edu.za>
2024-06-10 15:29:25 +0200

    exam

--- app/src/main/java/com/example/imad/MainActivity.kt
+++ app/src/main/java/com/example/imad/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.imad.R
 
+//uhhu
 class MainActivity : AppCompatActivity() {
     lateinit var MainScreen: Button
 
